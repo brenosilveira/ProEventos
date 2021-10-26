aca3efccd15a4f418fb6ae255222d178abbf1cb7
Breno Silveira <breno.assis2@hotmail.com>
2021-10-26 18:47:35 -0300

    3° Session - Angular Event Grid

--- front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,7 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any
+  public eventos: any = [];
+  eventosFiltrados: any = [];
+  widthImg: number = 200;
+  marginImg: number = 1;
+  showImg = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (eventos: any) => eventos.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+                        eventos.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -17,11 +39,18 @@ export class EventosComponent implements OnInit {
     this.getEventos()
   }
 
+  public showImage() {
+    this.showImg = !this.showImg;
+  }
+
 
   public getEventos(): void {
 
     this.http.get(`https://localhost:5001/api/eventos`).subscribe(
-      response => this.eventos = response,
+      response =>  {
+        this.eventos = response
+        this.eventosFiltrados = this.eventos
+      },
       error => console.log(error)
     )
   }
